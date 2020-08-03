732ac411004adba3ccc81764010201a154538a2e
gavadinov <gavadinov@gmail.com>
2020-08-03 11:43:35 +0100

    Add a RAII wrapper for the child process

--- server/tests/integ_test.rs
+++ server/tests/integ_test.rs
@@ -1,7 +1,25 @@
 use assert_cmd::prelude::*;
 use reqwest::blocking::get;
 use std::fs;
-use std::process::Command;
+use std::process::{Child, Command};
+
+struct ProcessGuard {
+    process: Child,
+}
+
+impl ProcessGuard {
+    fn new(process: Child) -> Self {
+        Self { process }
+    }
+}
+
+impl Drop for ProcessGuard {
+    // The Drop method is the destructor for the struct. It will be invoked when the struct is destroyed.
+    // https://en.wikipedia.org/wiki/Resource_acquisition_is_initialization
+    fn drop(&mut self) {
+        self.process.kill().unwrap();
+    }
+}
 
 // This function will be executed by the `cargo test` command.
 #[test]
@@ -11,15 +29,15 @@ fn test_hello_path() {
         fs::read_to_string(format!("{}/public/hello.html", env!("CARGO_MANIFEST_DIR"))).unwrap();
 
     // The `cargo_bin` function will find the executable for the `server` crate and the `spawn` method will run it in the background.
-    let mut test_bin = Command::cargo_bin("server").unwrap().spawn().unwrap();
+    // When the `_guard` variable goes out of scope the server process will be killed.
+    let _guard = ProcessGuard::new(Command::cargo_bin("server").unwrap().spawn().unwrap());
 
     // We make a GET request to the /hello path on the server. The `text` method will return the response body as a String.
     let response = get("http://127.0.0.1:8080/hello").unwrap().text().unwrap();
 
-    // We kill the server before we exit the test
-    test_bin.kill().unwrap();
-
     // The `assert_eq` macro will assert if both parameters are the same.
     // In this case the response body should be the same as the contents of the hello.html file.
     assert_eq!(expected_response, response);
+
+    // The `server` process will be killed when the current stackframe is released.
 }
